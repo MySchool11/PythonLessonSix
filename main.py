7252be3f093c7298e1ec8e74a154d26bac088d99
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-06-25 13:33:15 +0100

    3rd Commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 __author__ = "Mr Bancroft"
 
+# Python does not use a delimiter (symbol to identify the line end), instead it relies on correctly structuring the code
+# using indentations to show where related code is
+for i in range(1, 12):
+    print("i = {:3}".format(i))
+    print("i * 2 = {:3}".format(i * 2))
+    print("i * i (i squared) = {:3}".format(i ** 2) + "\n")
+print("\nLoop finished")
+
+# using the above block as an example, everything in the for loop is indented and everything outside of it is not run in
+# the loop hence the final print() statement runs only once
