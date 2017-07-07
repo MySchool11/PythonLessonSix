8084878bef57fe4e54c508023020637f6dba026f
slambo511 <sam@samuelbancroft.com>
2017-07-07 23:41:10 +0100

    Added bits

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ print("\nLoop finished")
 
 # using the above block as an example, everything in the for loop is indented and everything outside of it is not run in
 # the loop hence the final print() statement runs only once
+
+# flow can be controlled by ordering your statements correctly, if we want to ask the user their age, first we need
+# their name
+name = input("Please enter your name: ")
+age = int(input("Please enter your age {0}: ".format(name)))
+print("{0}, your age is {1}, correct?".format(name, age))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You will have to wait {0} more years to vote yet".format(18 - age))
+    print("Nevermind none of the choices right now are any good")
